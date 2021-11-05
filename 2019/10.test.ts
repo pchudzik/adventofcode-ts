f56245fdb5bb83f7244d1763356b81e7a80cb2d9
Paweł Chudzik <pawel.chudzik@gmail.com>
2021-11-05 21:52:26 +0100

    201910 part2

--- 2019/10.test.ts
+++ 2019/10.test.ts
@@ -1,5 +1,5 @@
 import { expect } from '@jest/globals';
-import { part1 } from './10';
+import { findAsteroidsEclipses, fireOrder, parsePuzzle, part1, part2 } from './10';
 
 describe('10.ts', () => {
   describe('part1', () => {
@@ -93,4 +93,102 @@ describe('10.ts', () => {
       expect(part1(puzzle)).toBe(210);
     });
   })
+
+  describe('part 2', () => {
+    it('sample 1', () => {
+      const puzzle = `
+        .#....#####...#..
+        ##...##.#####..##
+        ##...#...#.#####.
+        ..#.....X...###..
+        ..#.#.....#....##
+      `;
+
+      const parsed = findAsteroidsEclipses(parsePuzzle(puzzle)).filter(([p, _]) => p.x === 8 && p.y === 3)[0]
+      const destroyedAsteroids = fireOrder(parsed);
+
+      expect(destroyedAsteroids).toStrictEqual([
+        {x: 8, y: 1},   //1
+        {x: 9, y: 0},   //2
+        {x: 9, y: 1},   //3
+        {x: 10, y: 0},  //4
+        {x: 9, y: 2},   //5
+        {x: 11, y: 1},  //6
+        {x: 12, y: 1},  //7
+        {x: 11, y: 2},  //8
+        {x: 15, y: 1},  //9
+
+        {x: 12, y: 2},  //1
+        {x: 13, y: 2},  //2
+        {x: 14, y: 2},  //3
+        {x: 15, y: 2},  //4
+        {x: 12, y: 3},  //5
+        {x: 16, y: 4},  //6
+        {x: 15, y: 4},  //7
+        {x: 10, y: 4},  //8
+        {x: 4, y: 4},   //9
+
+        {x: 2, y: 4},   //1
+        {x: 2, y: 3},   //2
+        {x: 0, y: 2},   //3
+        {x: 1, y: 2},   //4
+        {x: 0, y: 1},   //5
+        {x: 1, y: 1},   //6
+        {x: 5, y: 2},   //7
+        {x: 1, y: 0},   //8
+        {x: 5, y: 1},   //9
+
+        {x: 6, y: 1},   //1
+        {x: 6, y: 0},   //2
+        {x: 7, y: 0},   //3
+        {x: 8, y: 0},   //4
+        {x: 10, y: 1},   //5
+        {x: 14, y: 0},   //6
+        {x: 16, y: 1},   //7
+        {x: 13, y: 3},   //8
+        {x: 14, y: 3},   //9
+      ]);
+    });
+
+
+    it('part2 sample 2', () => {
+      const puzzle = `
+      .#..##.###...#######
+      ##.############..##.
+      .#.######.########.#
+      .###.#######.####.#.
+      #####.##.#.##.###.##
+      ..#####..#.#########
+      ####################
+      #.####....###.#.#.##
+      ##.#################
+      #####.##.###..####..
+      ..######..##.#######
+      ####.##.####...##..#
+      .#####..#.######.###
+      ##...#.##########...
+      #.##########.#######
+      .####.#.###.###.#.##
+      ....##.##.###..#####
+      .#.#.###########.###
+      #.#.#.#####.####.###
+      ###.##.####.##.#..##
+    `;
+
+      const destroyedAsteroids = part2(puzzle);
+
+      expect(destroyedAsteroids[0]).toStrictEqual({x: 11, y: 12})
+      expect(destroyedAsteroids[1]).toStrictEqual({x: 12, y: 1})
+      expect(destroyedAsteroids[2]).toStrictEqual({x: 12, y: 2})
+
+      expect(destroyedAsteroids[9]).toStrictEqual({x: 12, y: 8})
+      expect(destroyedAsteroids[19]).toStrictEqual({x: 16, y: 0})
+      expect(destroyedAsteroids[49]).toStrictEqual({x: 16, y: 9})
+      expect(destroyedAsteroids[99]).toStrictEqual({x: 10, y: 16})
+      expect(destroyedAsteroids[198]).toStrictEqual({x: 9, y: 6})
+      expect(destroyedAsteroids[199]).toStrictEqual({x: 8, y: 2})
+      expect(destroyedAsteroids[200]).toStrictEqual({x: 10, y: 9})
+      expect(destroyedAsteroids[298]).toStrictEqual({x: 11, y: 1})
+    });
+  });
 });
--- 2019/10.ts
+++ 2019/10.ts
@@ -1,5 +1,3 @@
-import exp from 'constants';
-
 type Coordinate = {
   x: number,
   y: number
@@ -16,29 +14,44 @@ export function parsePuzzle(puzzle: string) {
 }
 
 function calculateAngle(start: Coordinate, other: Coordinate) {
-  //atan2
   const x = other.x - start.x
   const y = other.y - start.y
-  const result = Math.atan2(y, x) * 180 / Math.PI;
-  return result;
-  // return (other.y - start.y) / (other.x - start.x);
+  const result = Math.atan2(y, x) * 180 / Math.PI + 90;
+  if (result < 0) {
+    return 360 - Math.abs(result);
+  } else {
+    return result;
+  }
 }
 
 type MonitoringStationScanningResult = [Coordinate, Record<number, Coordinate[]>]
 
+function byDistance(center: Coordinate) {
+  function distance(p: Coordinate, q: Coordinate) {
+    return Math.sqrt(
+      Math.pow(p.x - q.x, 2) + Math.pow(p.y - q.y, 2),
+    );
+  }
+
+  return (a: Coordinate, b: Coordinate) => {
+    const aDistance = distance(a, center);
+    const bDistance = distance(b, center);
+    return aDistance - bDistance;
+  }
+}
+
 export function findAsteroidsEclipses(asteroids: Coordinate[]) {
   const eclipses: MonitoringStationScanningResult[] = [];
   for (const center of asteroids) {
-    // const center = {x:0, y: 0};
     const asteroidsAtTheAngle: Record<number, Coordinate[]> = {}
     for (const a of asteroids.filter(p => !(p.x === center.x && p.y === center.y))) {
       if (a.x === center.x && a.y === center.y) {
         continue;
       }
       const angle = calculateAngle(center, a)
-      // console.log(center, a,);
       const asteroids = asteroidsAtTheAngle[angle] || [];
       asteroids.push(a)
+      asteroids.sort(byDistance(center));
       asteroidsAtTheAngle[angle] = asteroids;
     }
     eclipses.push([center, asteroidsAtTheAngle]);
@@ -54,7 +67,37 @@ export function findBestMonitoringStationPosition(scanningResults: MonitoringSta
     .reduce((a, b) => a > b ? a : b);
 }
 
-export function part1(puzzle:string) {
+export function part1(puzzle: string) {
   const eclipses = findAsteroidsEclipses(parsePuzzle(puzzle));
   return findBestMonitoringStationPosition(eclipses);
+}
+
+export function part2(puzzle: string): Coordinate[] {
+  const eclipses = findAsteroidsEclipses(parsePuzzle(puzzle));
+  const asteroids = eclipses.reduce((a, b) => Object.keys(a[1]).length > Object.keys(b[1]).length ? a : b);
+  return fireOrder(asteroids)
+}
+
+
+export function fireOrder([_, asteroids]: MonitoringStationScanningResult): Coordinate[] {
+  function hasAnyAsteroidsToDestroy(values: Coordinate[][]) {
+    return values.map(a => a.length).some(a => a > 0);
+  }
+
+  const sortedAngles = Object
+    .keys(asteroids)
+    .map(a => parseFloat(a))
+    .sort((a, b) => a - b);
+
+  const result: Coordinate[] = []
+
+  while (hasAnyAsteroidsToDestroy(Object.values(asteroids))) {
+    for (const angle of sortedAngles) {
+      const asteroid = asteroids[angle].shift()
+      if (asteroid) {
+        result.push(asteroid)
+      }
+    }
+  }
+  return result;
 }
